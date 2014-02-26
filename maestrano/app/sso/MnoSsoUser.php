2ef599d385cb248b9739a8768d983735594c47b8
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-02-26 17:56:21 +1100

    configured mno api

--- maestrano/app/sso/MnoSsoUser.php
+++ maestrano/app/sso/MnoSsoUser.php
@@ -12,6 +12,12 @@ class MnoSsoUser extends MnoSsoBaseUser
    */
   public $connection = null;
   
+  /**
+   * SugarCRM unique key
+   * @var String
+   */
+  public $app_unique_key = '';
+  
   
   /**
    * Extend constructor to inialize app specific objects
@@ -27,6 +33,7 @@ class MnoSsoUser extends MnoSsoBaseUser
     
     // Assign new attributes
     $this->connection = $opts['db_connection'];
+    $this->app_unique_key = $opts['app_unique_key'];
   }
   
   
@@ -37,29 +44,25 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return boolean whether the user was successfully set in session or not
    */
-  // protected function setInSession()
-  // {
-  //   // First set $conn variable (need global variable?)
-  //   $conn = $this->connection;
-  //   
-  //   $sel1 = $conn->query("SELECT ID,name,lastlogin FROM user WHERE ID = $this->local_id");
-  //   $chk = $sel1->fetch();
-  //   if ($chk["ID"] != "") {
-  //       $now = time();
-  //       
-  //       // Set session
-  //       $this->session['userid'] = $chk['ID'];
-  //       $this->session['username'] = stripslashes($chk['name']);
-  //       $this->session['lastlogin'] = $now;
-  //       
-  //       // Update last login timestamp
-  //       $upd1 = $conn->query("UPDATE user SET lastlogin = '$now' WHERE ID = $this->local_id");
-  //       
-  //       return true;
-  //   } else {
-  //       return false;
-  //   }
-  // }
+  protected function setInSession()
+  {
+    // Retrieve the user
+    $user = new User();
+    $user->retrieve($this->local_id);
+    
+    if ($user && $user->id) {
+        
+        // Set session
+        $this->session['authenticated_user_id'] = $user->id;
+        $this->session['login_user_name'] = $user->user_name;
+		    $this->session['login_password'] = $user->user_hash;
+        $this->session['unique_key'] = $this->app_unique_key;
+        
+        return true;
+    } else {
+        return false;
+    }
+  }
   
   
   /**
@@ -69,52 +72,107 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return the ID of the user created, null otherwise
    */
-  // protected function createLocalUser()
-  // {
-  //   $lid = null;
-  //   
-  //   if ($this->accessScope() == 'private') {
-  //     // First set $conn variable (need global variable?)
-  //     $conn = $this->connection;
-  //     
-  //     // Create user
-  //     $lid = $this->connection->query("CREATE BLA.....");
-  //   }
-  //   
-  //   return $lid;
-  // }
+  protected function createLocalUser()
+  {
+    $lid = null;
+    
+    if ($this->accessScope() == 'private') {
+      // Build user
+      $user = $this->buildLocalUser();
+      
+      // Save user and retrieve local id
+      $user->save();
+      $lid = $user->id;
+    }
+    
+    return $lid;
+  }
+  
+  /**
+   * Build a local user for creation
+   *
+   * @return a SugarCRM user
+   */
+  protected function buildLocalUser()
+  {
+    $user = new User();
+    $user->user_name       = $this->uid;
+    $user->first_name      = $this->name;
+    $user->last_name       = $this->surname;
+    $user->status          = 'Active';
+    $user->is_admin        = $this->isUserAdmin();
+    $user->email1          = $this->email;
+    $user->employee_status = 'Active';
+    $user->user_hash       = md5($this->generatePassword());
+    
+    return $user;
+  }
+  
+  /**
+   * Return wether the user is admin or not
+   *
+   * @return integer 1 if admin and 0 otherwise
+   */
+  protected function isUserAdmin()
+  {
+    $is_admin = 0; // User
+    
+    if ($this->app_owner) {
+      $is_admin = 1; // Admin
+    } else {
+      foreach ($this->organizations as $organization) {
+        if ($organization['role'] == 'Admin' || $organization['role'] == 'Super Admin') {
+          $is_admin = 1;
+        } else {
+          $is_admin = 0;
+        }
+      }
+    }
+    
+    return $is_admin;
+  }
   
   /**
    * Get the ID of a local user via Maestrano UID lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function getLocalIdByUid()
-  // {
-  //   $result = $this->connection->query("SELECT ID FROM user WHERE mno_uid = {$this->connection->quote($this->uid)} LIMIT 1")->fetch();
-  //   
-  //   if ($result && $result['ID']) {
-  //     return $result['ID'];
-  //   }
-  //   
-  //   return null;
-  // }
+  protected function getLocalIdByUid()
+  {
+    $arg = addslashes($this->uid);
+    $result = $this->connection->query("SELECT id FROM users WHERE mno_uid = '{$arg}' LIMIT 1");
+    $result = $this->connection->fetchByAssoc($result);
+    
+    if ($result && $result['id']) {
+      return $result['id'];
+    }
+    
+    return null;
+  }
   
   /**
    * Get the ID of a local user via email lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function getLocalIdByEmail()
-  // {
-  //   $result = $this->connection->query("SELECT ID FROM user WHERE email = {$this->connection->quote($this->email)} LIMIT 1")->fetch();
-  //   
-  //   if ($result && $result['ID']) {
-  //     return $result['ID'];
-  //   }
-  //   
-  //   return null;
-  // }
+  protected function getLocalIdByEmail()
+  {
+    $arg = addslashes($this->email);
+    $result = $this->connection->query("SELECT u.id
+      FROM users u, email_addr_bean_rel r, email_addresses e
+      WHERE e.id = r.email_address_id
+      AND r.primary_address = 1
+      AND r.bean_module = 'Users'
+      AND u.id = r.bean_id
+      AND e.email_address = '{$arg}' LIMIT 1");
+    $result = $this->connection->fetchByAssoc($result);
+    
+    if ($result && $result['id']) {
+      return $result['id'];
+    }
+    
+    return null;
+  }
   
   /**
    * Set all 'soft' details on the user (like name, surname, email)
@@ -122,28 +180,61 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return boolean whether the user was synced or not
    */
-   // protected function syncLocalDetails()
-   // {
-   //   if($this->local_id) {
-   //     $upd = $this->connection->query("UPDATE user SET name = {$this->connection->quote($this->name . ' ' . $this->surname)}, email = {$this->connection->quote($this->email)} WHERE ID = $this->local_id");
-   //     return $upd;
-   //   }
-   //   
-   //   return false;
-   // }
+   protected function syncLocalDetails()
+   {
+     if($this->local_id) {
+       // Sync the user first
+       $data = Array(
+         'id'         => addslashes($this->local_id),
+         'user_name'  => addslashes($this->uid),
+         'first_name' => addslashes($this->name),
+         'last_name'  => addslashes($this->surname),
+         'email'      => addslashes($this->email),
+         'email_caps' => addslashes(strtoupper($this->email)),
+       );
+       
+       $upd1 = $this->connection->query("UPDATE users 
+         SET user_name = '{$data['user_name']}',
+         first_name = '{$data['first_name']}',
+         last_name = '{$data['last_name']}'
+         WHERE id = '{$data['id']}'");
+       
+       // Sync the user primary email then
+       $upd2 = $this->connection->query("UPDATE users u, email_addr_bean_rel r, email_addresses e
+         SET e.email_address = '{$data['email']}',
+         e.email_address_caps = '{$data['email_caps']}'
+         WHERE e.id = r.email_address_id
+         AND r.primary_address = 1
+         AND r.bean_module = 'Users'
+         AND u.id = r.bean_id
+         AND u.id = '{$data['id']}'");
+       
+       return $upd1 && $upd2;
+     }
+     
+     return false;
+   }
   
   /**
    * Set the Maestrano UID on a local user via id lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function setLocalUid()
-  // {
-  //   if($this->local_id) {
-  //     $upd = $this->connection->query("UPDATE user SET mno_uid = {$this->connection->quote($this->uid)} WHERE ID = $this->local_id");
-  //     return $upd;
-  //   }
-  //   
-  //   return false;
-  // }
+  protected function setLocalUid()
+  {
+    if($this->local_id) {
+      $data = Array(
+        'id'       => addslashes($this->local_id),
+        'mno_uid'  => addslashes($this->uid),
+      );
+      
+      $upd = $this->connection->query("UPDATE users 
+        SET mno_uid = '{$data['mno_uid']}'
+        WHERE id = '{$data['id']}'");
+      
+      return $upd;
+    }
+    
+    return false;
+  }
 }
