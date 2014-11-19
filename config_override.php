74af1c46dbed3c003a5efac5ef03532fc7646cbe
root <root@bruno-VirtualBox.(none)>
2014-11-19 16:26:55 +1100

    Resinstall OQC module

---
+++ config_override.php
@@ -0,0 +1,14 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['http_referer']['list']['0'] = 'application.maestrano.io';
+$sugar_config['http_referer']['list']['1'] = 'uat.maestrano.io';
+$sugar_config['http_referer']['list']['2'] = 'maestrano.com';
+$sugar_config['addAjaxBannedModules']['0'] = 'oqc_TextBlock';
+$sugar_config['addAjaxBannedModules']['1'] = 'oqc_Offering';
+$sugar_config['addAjaxBannedModules']['2'] = 'oqc_Product';
+$sugar_config['addAjaxBannedModules']['3'] = 'oqc_Contract';
+$sugar_config['addAjaxBannedModules']['4'] = 'oqc_ExternalContract';
+$sugar_config['addAjaxBannedModules']['5'] = 'oqc_ProductCatalog';
+$sugar_config['addAjaxBannedModules']['6'] = 'oqc_Addition';
+$sugar_config['addAjaxBannedModules']['7'] = 'oqc_Task';
+/***CONFIGURATOR***/
---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2014-11-19 10:04:07
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
